c84df3735545081b97ecf80c3fe88261440ade36
Rad <47281323+killadog@users.noreply.github.com>
2022-01-14 21:17:43 +0300

    upd

--- pingonator.ps1
+++ pingonator.ps1
@@ -3,7 +3,7 @@
     Pingonator
 .EXAMPLE
     Ping network 192.168.0.0 in range 192.168.0.1-192.168.0.254
-    .\pingonator.ps1 -net 192.168.0 -start 1 -end 254 -count 1 -resolve 1 -mac 1 -latency 1 -grid 1 -ports 25,80 -exclude 3,4
+    .\pingonator.ps1 -net 192.168.0 -start 1 -end 254 -count 1 -resolve 1 -mac 1 -latency 1 -grid 1 -ports 20-23,25,80 -exclude 3,4,9-12
     or
     .\pingonator.ps1 -net 192.168.0
 .NOTES
@@ -68,24 +68,34 @@ if ($start -gt $end) {
     exit;
 }
 
-$ports = $ports -replace (' ', '')
+$ports_list = @()
+#$ports = $ports -replace (' ', '')
 foreach ($p in $ports) {
-    if ($p -notmatch '^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])') {
-        Write-Error "Not valid ports! Syntax: 25,80,135"
+    if (($p -match '^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])$') -or 
+    ($p -match '^(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])-(6553[0-5]|655[0-2][0-9]|65[0-4][0-9]{2}|6[0-4][0-9]{3}|[1-5][0-9]{4}|[1-9][0-9]{1,3}|[0-9])$')) {
+        if ($p -like "*-*") {
+            $splitter = $p.split("-")
+            $splitter_array = $splitter[0]..$splitter[1]
+            $ports_list += $splitter_array 
+        }
+        else {
+            $ports_list += $p
+        }
+    }
+    else {
+        Write-Error "Not valid ports! Syntax: 25,80,135,1096-2048"
         exit;    
     }
 }
+$ports_list = $ports_list | Select-Object -Unique
 
-#$exclude_list = @()
-#$exclude = $exclude -replace (' ', '')
+$exclude_list = @()
 foreach ($e in $exclude) {
     #$e = $e -replace (' ', '')
-    #Write-Host $e
     if (($e -match '^(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)$') -or ($e -match '^(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)-(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)$')) {
         if ($e -like "*-*") {
             $splitter = $e.split("-")
             $splitter_array = $splitter[0]..$splitter[1]
-            
             $exclude_list += $splitter_array 
         }
         else {
@@ -97,11 +107,7 @@ foreach ($e in $exclude) {
         exit;    
     }
 }
-
-Write-Host $exclude_list
-#$exclude_list = $exclude_list | Select-Object -Unique
-Write-Host $exclude_list
-#exit
+$exclude_list = $exclude_list | Select-Object -Unique
 function ColorValue {
     param (     
         [Parameter(Mandatory = $False)][string]$Column_Name,
@@ -112,7 +118,7 @@ function ColorValue {
 }
 
 $live_ips = @()
-$range = $end - $start + 1
+$range = $end - $start + 1 - $exclude_list.Count
 [ref]$counter = 0
 $now = Get-Date -UFormat "%Y/%m/%d-%H:%M:%S"
 Write-Host "Starting at $now"
@@ -146,12 +152,12 @@ $ping_time = Measure-Command {
                 if ($using:latency -eq 1) {
                     $ms = $ping.Latency
                 }
-                <# $test = $using:ports #>
-                if ($($using:ports) -ne 0) { 
-                    ForEach ($p in $($using:ports)) {
+
+                if ($($using:ports_list) -ne 0) { 
+                    ForEach ($p in $($using:ports_list)) {
                         $check_port = Test-NetConnection -ComputerName $ip -InformationLevel Quiet -Port $p -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
                         if ($check_port) {
-                            $open_ports += $p + " "
+                            $open_ports += "$p "
                         }
                     } 
                 }
@@ -168,14 +174,14 @@ $ping_time = Measure-Command {
         return $ip_list
     } 
     
-    $live_ips = $pingout.count
+    $live_ips = $($pingout.'IP address').count
 
     $pingout = $pingout | Sort-Object { $_.'IP Address' -as [Version] } 
     
     $pingout | Format-Table -Property @{name = "IP address"; Expression = { ColorValue $_.'IP address' 92 } },
     @{name = "Name"; Expression = { ColorValue $_.Name 93 } },
     @{name = "MAC address"; Expression = { ColorValue $_.'MAC address' 97 } },
-    @{name = "Latency (ms)"; Expression = { if ($_.'Latency (ms)' -gt 100) { ColorValue $_.Latency 91 } else { ColorValue $_.'Latency (ms)' 93 } }; align = 'center' },
+    @{name = "Latency (ms)"; Expression = { if ($_.'Latency (ms)' -gt 100) { ColorValue $_.'Latency (ms)' 91 } else { ColorValue $_.'Latency (ms)' 93 } }; align = 'center' },
     @{name = "Open Ports"; Expression = { ColorValue $_.'Open ports' 92 }; align = 'center' } | Out-Default
 }
 
